e432bd46c0d018c27b6968e4527df31dea63c719
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-03-09 16:10:25 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Created by: Ms Raffin
+// Created on: March 09 2022
+// This file contains the JS functions for index.html
+
+// this function displays "Hello, World!"
+function myButtonClicked () {
+  
+  
+  document.getElementById('hello-world').innerHTML = 'The power of JS!'
+}
